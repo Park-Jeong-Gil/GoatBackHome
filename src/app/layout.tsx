348c9df236fc83ac5c9d489578d09eb0eb0f4732
girgir <wjdrlf5986@naver.com>
2026-02-02 12:52:53 +0900

    메타 정보 업데이트

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,27 @@ export const metadata: Metadata = {
   icons: {
     icon: "/goat.ico",
   },
+  openGraph: {
+    title: "Goat Back Home",
+    description:
+      "Become a mountain goat and climb the treacherous mountains to find your way back home!",
+    images: [
+      {
+        url: "/thumb.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Goat Back Home",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Goat Back Home",
+    description:
+      "Become a mountain goat and climb the treacherous mountains to find your way back home!",
+    images: ["/thumb.jpg"],
+  },
 };
 
 export default function RootLayout({
